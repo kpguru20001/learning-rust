8ea1c57f6cd29f66b83d7261745fd56646dc795b
gauravkp <kpguru20001@gmail.com>
2024-04-05 17:31:07 +0530

    ✨ refactor: improve vectors handling and tuple indexing
    - Refactored vec_loop function to multiply each element in the vector by 2
    - Refactored vec_map function to return a new vector with elements multiplied by 2
    - Updated array_and_vec function to create a vector from a plain array
    - Improved tuple indexing in indexing_tuple function
    - Updated pattern matching in main function for primitive_types5

--- rustlings/exercises/primitive_types/primitive_types3.rs
+++ rustlings/exercises/primitive_types/primitive_types3.rs
@@ -5,13 +5,19 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+fn get_array() -> Vec<i32> {
+    let mut v = Vec::new();
+    for i in 1..=1001 {
+        v.push(i)
+    }
+    return v;
+}
 
 fn main() {
-    let a = ???
+    let a = get_array();
 
     if a.len() >= 100 {
-        println!("Wow, that's a big array!");
+        println!("Wow, that's a big array! {}",a.len());
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
         panic!("Array not big enough, more elements needed")
